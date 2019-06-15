140420ccac7758ebdaed8b581a1f31d0805ac4ab
mxdh <20399194+mxdh@users.noreply.github.com>
2019-06-15 15:11:52 +0800

    update

--- code/00077/19-6.cpp
+++ code/00077/19-6.cpp
@@ -1,6 +1,28 @@
+// AC
 #include <cstdio>
 
+const int N = 14;
+int a[N], n;
+bool flag;
+
+void dfs(int k, bool s, int val) {
+    if (!val && s) {
+        flag = true;
+        return;
+    }
+    if (k == n) return;
+    if (!flag) dfs(k + 1, s, val);
+    if (!flag) dfs(k + 1, true, val ^ a[k]);
+}
+
 int main() {
-    scanf("%d", &n);
+    int q = 10;
+    while (q--) {
+        scanf("%d", &n);
+        for (int i = 0; i < n; ++i) scanf("%d", a + i);
+        flag = false;
+        dfs(0, 0, 0);
+        puts(flag ? "NO" : "YES");
+    }
     return 0;
 }
