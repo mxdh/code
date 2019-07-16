eca1d359f44f8f0a829a562ecd84acf514e1a22c
mxdh <20399194+mxdh@users.noreply.github.com>
2019-07-17 00:20:21 +0800

    update

--- code/00095/19-7.cpp
+++ code/00095/19-7.cpp
@@ -6,5 +6,12 @@ int main() {
     while (t--) {
         int n, m;
         scanf("%d%d", &n, &m);
+        for (int i = 0; i < m; ++i) {
+            int u, v;
+            scanf("%d%d", &u, &v);
+            AddEdge(v - 1, u - 1);
+        }
+        for (int i = 0; i < n; ++i)
+            if (!ind[i]) q[r++] = i;
     }
 }
