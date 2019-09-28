fcb1e91fd1b5ed9d3cfe62cd09446d0cb25c1483
mxdh <20399194+mxdh@users.noreply.github.com>
2019-09-28 12:19:51 +0800

    update

--- code/00119/19-9.cpp
+++ code/00119/19-9.cpp
@@ -4,98 +4,112 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::fixed;
 using std::setprecision;
 
 typedef long long LL;
 
-const int N = 100000, INF = 2147483646;
+const int INF = 2147483646;
+
+template <typename _Tp>
+struct segment {
+    _Tp val;
+    int l, r;
+};
 
 class segment_tree {
   private:
     struct _node {
         LL val, tag;
-        int l, r;
+        const int l, r;
         _node *ls, *rs;
 
-        _node() : ls(NULL), rs(NULL), val(0), tag(0) {}
-    } root;
+        _node(int _l, int _r)
+            : val(0), tag(0), l(_l), r(_r), ls(NULL), rs(NULL) {}
+    } * root;
 
     void _prepare(_node *&k, int l, int r) {
-        if (k == NULL) {
-            k = new _node;
-            k->l = l;
-            k->r = r;
+        if (k == NULL) k = new _node(l, r);
+    }
+
+    void __push_down(_node *k) {
+        _node *ls = k->ls, *rs = k->rs;
+        LL &tag = k->tag;
+        ls->val += tag * ((LL)((k->r - k->l) >> 1) + 1);
+        ls->tag += tag;
+        rs->val += tag * ((LL)(k->r - k->l + 1) >> 1);
+        rs->tag += tag;
+        tag = 0;
+    }
+
+    void _push_down(_node *k) {
+        if (k->tag) {
+            _prepare(k->ls, k->l, (k->l + k->r) >> 1);
+            _prepare(k->rs, ((k->l + k->r) >> 1) + 1, k->r);
+            __push_down(k);
         }
     }
 
-    void _push_down(_node *k, int mid) {
-        _node *ls = k->ls, *rs = k->rs;
-        LL tag = k->tag;
-        _prepare(ls, k->l, mid);
-        ls->val += tag * ((LL)mid - k->l + 1);
-        ls->tag += tag;
-        _prepare(rs, mid + 1, k->r);
-        rs->val += tag * ((LL)k->r - mid);
-        rs->tag += tag;
+    void _push_up(_node *k) {
+        k->val = 0;
+        if (k->ls != NULL) k->val += k->ls->val;
+        if (k->rs != NULL) k->val += k->rs->val;
     }
-
-    void _push_up(_node *k) { k->val = k->ls->val + k->rs->val; }
 
     void _modify(_node *k, int l, int r, int val) {
         int l_ = k->l, r_ = k->r;
         if (l_ >= l && r_ <= r) {
-            k->val += (LL)val * ((LL)r - l + 1);
+            k->val += (LL)val * ((LL)r_ - l_ + 1);
             k->tag += val;
             return;
         }
         int mid = (l_ + r_) >> 1;
-        _push_down(k, mid);
+        _push_down(k);
         if (mid >= l) {
             _prepare(k->ls, l_, mid);
-            _modify(k->ls, l, mid, val);
+            _modify(k->ls, l, r, val);
         }
         if (mid < r) {
             _prepare(k->rs, mid + 1, r_);
-            _modify(k->rs, mid + 1, r, val);
+            _modify(k->rs, l, r, val);
         }
         _push_up(k);
     }
 
     LL _query(_node *k, int l, int r) {
         int l_ = k->l, r_ = k->r;
-        LL ret = 0;
         if (l_ >= l && r_ <= r) return k->val;
-        int mid = (l_ + r_) >> 1;
-        _push_down(k, mid);
-        if (mid >= l && k->ls != NULL) ret += _query(k->ls, l, mid);
-        if (mid < r && k->rs != NULL) ret += _query(k->rs, mid + 1, r);
+        LL ret = 0;
+        _push_down(k);
+        if ((l_ + r_) >> 1 >= l && k->ls != NULL) ret += _query(k->ls, l, r);
+        if ((l_ + r_) >> 1 < r && k->rs != NULL) ret += _query(k->rs, l, r);
         return ret;
     }
 
   public:
-    segment_tree(int l = 0, int r = INF) {
-        root.l = l;
-        root.r = r;
-    }
+    segment_tree(int l = 0, int r = INF) { _prepare(root, l, r); }
 
-    void modify(int l, int r, int val) { _modify(&root, l, r, val); }
+    void modify(int l, int r, int val) { _modify(root, l, r, val); }
 
-    LL query(int l, int r) { return _query(&root, l, r); }
-} seg;
-
-int a[N], b[N];
+    LL query(int l, int r) { return _query(root, l, r); }
+} seg(1, INF);
 
 int main() {
     int n, m;
     cin >> n >> m;
-    for (int i = 0; i < n; ++i) cin >> a[i] >> b[i];
+    for (int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
+        seg.modify(a, INF, b);
+    }
     for (int i = 0; i < m; ++i) {
         int op, a, b;
         cin >> op >> a >> b;
         if (op == 1)
-            cout << setprecision(4) << seg.query(a, b) << endl;
+            cout << fixed << setprecision(4)
+                 << (double)seg.query(a, b) / (b - a + 1) << endl;
         else
-            seg.modify()
+            seg.modify(a, INF, b);
     }
     return 0;
 }
