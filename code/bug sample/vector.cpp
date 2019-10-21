706d30899949f8363f0d15af1f743ad026ae1d6c
mxdh <20399194+mxdh@users.noreply.github.com>
2019-10-21 18:15:51 +0800

    update

---
+++ code/bug sample/vector.cpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+int main() {
+    std::vector<int> v;
+    return 0;
+}
--- template/线段树/数状数组.cpp
+++ template/线段树/数状数组.cpp
@@ -1,25 +1,23 @@
-class BIT
-{
-public:
-	BIT(int n):tr(new int[n+1])
-	{
-		this->n=n;
-		memset(tr,0,(n+1)*sizeof *tr);
-	}
-	void change(int k,int num)
-	{
-		for (;k<=n;k+=low_bit(k)) tr[k]+=num;
-	}
-	int get_sum(int k)
-	{
-		int sum=0;
-		for (;k;k-=low_bit(k)) sum+=tr[k];
-		return sum;
-	}
-private:
-	int low_bit(int num)
-	{
-		return num&(num^(num-1)); //num&(-num)
-	}
-	int n,*tr;
+#include <cstring>
+
+class BIT {
+  public:
+    BIT(int n) : tr(new int[n + 1]) {
+        this->n = n;
+        memset(tr, 0, (n + 1) * sizeof *tr);
+    }
+    void change(int k, int num) {
+        for (; k <= n; k += low_bit(k)) tr[k] += num;
+    }
+    int get_sum(int k) {
+        int sum = 0;
+        for (; k; k -= low_bit(k)) sum += tr[k];
+        return sum;
+    }
+
+  private:
+    int low_bit(int num) {
+        return num & (num ^ (num - 1));  // num&(-num)
+    }
+    int n, *tr;
 };
